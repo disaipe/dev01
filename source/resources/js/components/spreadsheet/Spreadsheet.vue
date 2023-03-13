817cd58914d18159d8b578c141fb4367a00a2616
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-03-13 21:38:25 +0300

    chore: spreadsheet improvements

--- source/resources/js/components/spreadsheet/Spreadsheet.vue
+++ source/resources/js/components/spreadsheet/Spreadsheet.vue
@@ -1,95 +1,94 @@
 <template lang='pug'>
 .spreadsheet-wrapper.h-full
     .relative.z-10
-        slot(name='toolbar')
-            .flex.space-x-2.mb-1(v-if='showToolbar')
-                el-dropdown(trigger='click')
-                    el-button(size='small' icon='arrow-down') Действия
-                    template(#dropdown)
-                        el-dropdown-menu
-                            slot(name='actions-menu-items')
-                            el-dropdown-item(@click='download') Сохранить в файл
-                            el-dropdown-item(@click='uploader.$el.querySelector("input").click()') Загрузить из файла
+        el-config-provider(size='small')
+            slot(name='toolbar')
+                .flex.space-x-2.mb-1(v-if='showToolbar')
+                    el-dropdown(trigger='click')
+                        el-button(size='small' icon='arrow-down') Действия
+                        template(#dropdown)
+                            el-dropdown-menu
+                                slot(name='actions-menu-items')
+                                el-dropdown-item(@click='download') Сохранить в файл
+                                el-dropdown-item(@click='uploader.$el.querySelector("input").click()') Загрузить из файла
 
-                el-select.w-32(
-                    v-model='data.fontFamilyInput'
-                    size='small'
-                    filterable
-                    allow-create
-                    placeholder=' '
-                    @change='setFontFamily'
-                )
-                    el-option(
-                        v-for='fontFamily of defaultFontFamilies'
-                        :key='fontFamily'
-                        :label='fontFamily'
-                        :value='fontFamily'
+                    el-select.w-32(
+                        v-model='data.fontFamilyInput'
+                        filterable
+                        allow-create
+                        placeholder=' '
+                        @change='setFontFamily'
                     )
-                el-select.w-16(
-                    v-model='data.fontSizeInput'
-                    size='small'
-                    filterable
-                    allow-create
-                    placeholder=' '
-                    @change='setFontSize'
-                )
-                    el-option(
-                        v-for='fontSize of defaultFontSizes'
-                        :key='fontSize'
-                        :label='fontSize'
-                        :value='fontSize'
-                    )
-                el-button-group(v-if='instance' size='small')
-                    //- el-button(@click='history.undo()') Undo
-                    //- el-button(@click='history.redo()') Redo
-                    el-button(
-                        :class='{ active: data.fontItalic }'
-                        @click='setItalic(!data.fontItalic)'
-                    )
-                        Icon(icon='material-symbols:format-italic-rounded' height='14')
-                    el-button(
-                        :class='{ active: data.fontBold }'
-                        @click='setBold(!data.fontBold)'
-                    )
-                        Icon(icon='material-symbols:format-bold-rounded' height='14')
-                    el-button(@click='() => borderDrop.handleOpen()')
-                        Icon(icon='material-symbols:border-all-outline-sharp' height='14')
-                        el-dropdown(
-                            ref='borderDrop'
-                            trigger='click'
+                        el-option(
+                            v-for='fontFamily of defaultFontFamilies'
+                            :key='fontFamily'
+                            :label='fontFamily'
+                            :value='fontFamily'
                         )
-                            span
-                            template(#dropdown)
-                                .grid.grid-cols-5.p-1.gap-1
-                                    el-link(@click='setBorder("all")')
-                                        Icon.text-xl(icon='material-symbols:border-all-outline')
-                                    el-link(@click='setBorder("inner")' disabled)
-                                        Icon.text-xl(icon='material-symbols:border-inner')
-                                    el-link(@click='setBorder("horizontal")' disabled)
-                                        Icon.text-xl(icon='material-symbols:border-horizontal')
-                                    el-link(@click='setBorder("vertical")' disabled)
-                                        Icon.text-xl(icon='material-symbols:border-vertical')
-                                    el-link(@click='setBorder("outer")' disabled)
-                                        Icon.text-xl(icon='material-symbols:border-outer')
+                    el-select.w-16(
+                        v-model='data.fontSizeInput'
+                        filterable
+                        allow-create
+                        placeholder=' '
+                        @change='setFontSize'
+                    )
+                        el-option(
+                            v-for='fontSize of defaultFontSizes'
+                            :key='fontSize'
+                            :label='fontSize'
+                            :value='fontSize'
+                        )
+                    el-button-group(v-if='instance')
+                        //- el-button(@click='history.undo()') Undo
+                        //- el-button(@click='history.redo()') Redo
+                        el-button(
+                            :class='{ active: data.fontItalic }'
+                            @click='setItalic(!data.fontItalic)'
+                        )
+                            Icon(icon='material-symbols:format-italic-rounded' height='14')
+                        el-button(
+                            :class='{ active: data.fontBold }'
+                            @click='setBold(!data.fontBold)'
+                        )
+                            Icon(icon='material-symbols:format-bold-rounded' height='14')
+                        el-button(@click='() => borderDrop.handleOpen()')
+                            Icon(icon='material-symbols:border-all-outline-sharp' height='14')
+                            el-dropdown(
+                                ref='borderDrop'
+                                trigger='click'
+                            )
+                                span
+                                template(#dropdown)
+                                    .grid.grid-cols-5.p-1.gap-1
+                                        el-link(@click='setBorder("all")')
+                                            Icon.text-xl(icon='material-symbols:border-all-outline')
+                                        el-link(@click='setBorder("inner")' disabled)
+                                            Icon.text-xl(icon='material-symbols:border-inner')
+                                        el-link(@click='setBorder("horizontal")' disabled)
+                                            Icon.text-xl(icon='material-symbols:border-horizontal')
+                                        el-link(@click='setBorder("vertical")' disabled)
+                                            Icon.text-xl(icon='material-symbols:border-vertical')
+                                        el-link(@click='setBorder("outer")' disabled)
+                                            Icon.text-xl(icon='material-symbols:border-outer')
 
-                                    el-link(@click='setBorder("left")')
-                                        Icon.text-xl(icon='material-symbols:border-left')
-                                    el-link(@click='setBorder("top")')
-                                        Icon.text-xl(icon='material-symbols:border-top')
-                                    el-link(@click='setBorder("right")')
-                                        Icon.text-xl(icon='material-symbols:border-right')
-                                    el-link(@click='setBorder("bottom")')
-                                        Icon.text-xl(icon='material-symbols:border-bottom')
-                                    el-link(@click='setBorder("none")')
-                                        Icon.text-xl(icon='material-symbols:border-clear')
+                                        el-link(@click='setBorder("left")')
+                                            Icon.text-xl(icon='material-symbols:border-left')
+                                        el-link(@click='setBorder("top")')
+                                            Icon.text-xl(icon='material-symbols:border-top')
+                                        el-link(@click='setBorder("right")')
+                                            Icon.text-xl(icon='material-symbols:border-right')
+                                        el-link(@click='setBorder("bottom")')
+                                            Icon.text-xl(icon='material-symbols:border-bottom')
+                                        el-link(@click='setBorder("none")')
+                                            Icon.text-xl(icon='material-symbols:border-clear')
 
-                    slot(name='toolbar-actions')
+                        slot(name='toolbar-actions')
 
-                .flex-1
-                    slot(name='toolbar-center')
+                    .flex-1
+                        slot(name='toolbar-center')
 
-                el-button-group(size='small')
-                    slot(name='toolbar-extra-actions')
+                    el-button-group
+                        slot(name='toolbar-extra-actions')
 
     el-upload.hidden(
         ref='uploader'
