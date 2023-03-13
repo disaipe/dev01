53810dc19cc6fc5d485f32926f9286cdfe53a139
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-03-13 21:28:51 +0300

    fix: table filter

--- source/resources/js/components/table/TableFilter.vue
+++ source/resources/js/components/table/TableFilter.vue
@@ -9,29 +9,55 @@ el-popover(
             :type='isSet ? "primary" : "default"'
             @click='toggleFilter'
         )
-            el-icon
-                Filter
+            icon(:icon='isSet ? "clarity:filter-off-line" : "clarity:filter-line"')
+
     .flex.flex-col.space-y-1(v-click-outside='{ handler: close, isActive: isVisible }')
-        el-input(v-model='filterStore.inputs[field]')
+
+        //- RELATED SELECTION
+        el-select(
+            v-if='schema && schema.relation'
+            v-model='filterStore.inputs[remoteField]'
+            filterable
+            @visible-change='(visible) => visible && loadRelatedOptions()'
+        )
+            el-option(
+                v-for='relatedOption of relatedOptions'
+                :key='relatedOption.$getKey()'
+                :label='relatedOption.$getName()'
+                :value='relatedOption.$getKey()'
+            )
+
+        //- BOOLEAN INPUT
+        el-switch(
+            v-else-if='schema && schema.type === "boolean"'
+            v-model='filterStore.inputs[remoteField]'
+        )
+
+        //- PLAIN INPUT
+        el-input(
+            v-else
+            v-model='filterStore.inputs[remoteField]'
+        )
 
         div
             el-button.w-full(
                 size='small'
                 :disabled='!isCanSet'
-                @click.stop='applyFilter(filterStore.inputs[field])'
+                @click.stop='applyFilter(filterStore.inputs[remoteField])'
             ) Применить
 
         div
             el-link.w-full(
                 :underline='false'
-                :disabled='!filterStore.inputs[field]'
+                :disabled='!filterStore.inputs[remoteField]'
                 @click.stop='applyFilter()'
             )
                 .text-xs Сбросить
 </template>
 
 <script>
-import { toRef, computed, inject } from 'vue';
+import { ref, toRef, computed, inject } from 'vue';
+import { useRepos } from '../../store/repository';
 
 export default {
     name: 'TableFilter',
@@ -40,40 +66,71 @@ export default {
         field: {
             type: String,
             required: true
+        },
+        schema: {
+            type: Object,
+            default: () => ({})
         }
     },
     emits: ['filter-change'],
     setup(props, { emit }) {
         const field = toRef(props, 'field');
+        const schema = toRef(props, 'schema');
         const { filterStore } = inject('TableInstance');
 
-        const value = toRef(filterStore.filters, field.value);
+        const remoteField = computed(() => {
+            if (schema.value) {
+                if (schema.value.relation) {
+                    return schema.value.relation.key || field.value;
+                }
+            }
+
+            return field.value;
+        });
+
+        const value = toRef(filterStore.filters, remoteField.value);
 
         const applyFilter = (value = undefined) => {
-            filterStore.visibility[field.value] = false;
-            filterStore.inputs[field.value] = undefined;
-            filterStore.filters[field.value] = value;
+            filterStore.visibility[remoteField.value] = false;
+            filterStore.inputs[remoteField.value] = undefined;
+            filterStore.filters[remoteField.value] = value;
 
-            emit('filter-change', field.value, value);
+            emit('filter-change', remoteField.value, value);
+        };
+
+        const relatedOptions = ref([]);
+
+        const loadRelatedOptions = () => {
+            if (schema.value?.relation?.model) {
+                const relatedRepo = useRepos()[schema.value.relation.model];
+                if (relatedRepo) {
+                    relatedOptions.value = relatedRepo.query().all();
+                }
+            }
         };
 
         return {
+            remoteField,
+
+            relatedOptions,
+            loadRelatedOptions,
+
             value,
             filterStore,
             applyFilter,
-            isSet: computed(() => filterStore.filters[field.value]),
-            isCanSet: computed(() => filterStore.inputs[field.value] !== filterStore.filters[field.value]),
-            isVisible: computed(() => filterStore.visibility[field.value] || false),
+            isSet: computed(() => filterStore.filters[remoteField.value]),
+            isCanSet: computed(() => filterStore.inputs[remoteField.value] !== filterStore.filters[remoteField.value]),
+            isVisible: computed(() => filterStore.visibility[remoteField.value] || false),
             onShow() {
-                if (!filterStore.inputs[field.value]) {
-                    filterStore.inputs[field.value] = filterStore.filters[field.value];
+                if (!filterStore.inputs[remoteField.value]) {
+                    filterStore.inputs[remoteField.value] = filterStore.filters[remoteField.value];
                 }
             },
             toggleFilter() {
-                filterStore.visibility[field.value] = !filterStore.visibility[field.value];
+                filterStore.visibility[remoteField.value] = !filterStore.visibility[remoteField.value];
             },
             close() {
-                filterStore.visibility[field.value] = false;
+                filterStore.visibility[remoteField.value] = false;
             }
         };
     }
