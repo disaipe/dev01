1dcd88b48132f584baa44617c89d5244a117b4d9
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-03 20:53:42 +0300

    feat: supervisord and queue service

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 if [ "$#" -eq 0 ]; then
-  chmod 0644 /etc/cron.d/scheduler && crontab /etc/cron.d/scheduler
+  chmod 0644 /etc/cron.d/scheduler && crontab -u www-data /etc/cron.d/scheduler
   service cron start
 
-  /bin/sh -c /usr/bin/php-fpm -o
+  # start fpm
+  /usr/bin/supervisord
 fi
 
 exec "$@"
