558765dad574d1955b86e4d474974617532b4ba4
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-04 20:31:43 +0300

    feat: module scheduled jobs worker

--- source/app/Console/Kernel.php
+++ source/app/Console/Kernel.php
@@ -2,19 +2,27 @@
 
 namespace App\Console;
 
+use App\Core\Module\Module;
+use App\Core\Module\ModuleManager;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected Schedule $schedule;
+
     /**
      * Define the application's command schedule.
      *
+     * @param Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $this->schedule = $schedule;
+
+        // Schedule jobs from modules
+        $this->scheduleModules();
     }
 
     /**
@@ -22,10 +30,29 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    /**
+     * Schedule jobs from active modules
+     *
+     * @return void
+     */
+    private function scheduleModules(): void {
+        /** @var ModuleManager $modules */
+        $modulesManager = app('modules');
+
+        if ($modulesManager) {
+            /** @var Module[] $modules */
+            $modules = $modulesManager->getModules(true);
+
+            foreach ($modules as $module) {
+                $module->getProvider()->schedule($this->schedule);
+            }
+        }
     }
 }
--- source/app/Core/Module/ModuleScheduledJob.php
+++ source/app/Core/Module/ModuleScheduledJob.php
@@ -2,31 +2,98 @@
 
 namespace App\Core\Module;
 
+use App\Core\Enums\JobProtocolState;
+use App\Models\JobProtocol;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
-abstract class ModuleScheduledJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
+abstract class ModuleScheduledJob implements ShouldQueue //, ShouldBeUniqueUntilProcessing
 {
-    use Queueable;
+    use Dispatchable, Queueable, InteractsWithQueue;
 
     protected Module $module;
 
-    abstract public function handle();
+    protected string $jobId;
 
-    public function enabled(): bool
+    public ?string $description = null;
+
+    abstract public function work(): mixed;
+
+    /**
+     * The number of seconds after which the job's unique lock will be released.
+     */
+    public int $uniqueFor = 3600;
+
+    public function __construct()
     {
-        return true;
+        $this->jobId = Str::uuid();
     }
 
-    protected function getModule(): ?Module
+    public final function handle()
+    {
+        $uuid = $this->job?->uuid();
+
+        if ($uuid) {
+            JobProtocol::query()
+                ->where('uuid', '=', $uuid)
+                ->update([
+                    'state' => JobProtocolState::Work,
+                    'started_at' => Carbon::now()
+                ]);
+        }
+
+        $failed = false;
+
+         try {
+            $result = $this->work();
+        } catch (\Exception $e) {
+            $failed = true;
+            $result = [
+              'error' => $e->getMessage(),
+              'stacktrace' => $e->getTraceAsString()
+            ];
+
+            Log::error($e);
+        }
+
+        if ($uuid) {
+            JobProtocol::query()
+                ->where('uuid', '=', $uuid)
+                ->update([
+                    'result' => $result,
+                    'state' => $failed ? JobProtocolState::Failed : JobProtocolState::Ready,
+                    'ended_at' => Carbon::now()
+                ]);
+        }
+    }
+
+    /**
+     * The unique ID of the job.
+     */
+    public function uniqueId(): string
+    {
+        return get_class($this);
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function getModule(): ?Module
     {
         $modules = app('modules');
 
         return $modules->getByNamespace(get_class($this));
     }
 
-    protected function getModuleConfig(): array
+    public function getModuleConfig(): array
     {
         $module = $this->getModule();
 
