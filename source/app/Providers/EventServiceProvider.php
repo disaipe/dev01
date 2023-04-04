dd86850c9dac75dad1bded91bff05d840debfad9
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-04 20:33:45 +0300

    feat: database queue for scheduled jobs

--- source/app/Providers/EventServiceProvider.php
+++ source/app/Providers/EventServiceProvider.php
@@ -2,11 +2,17 @@
 
 namespace App\Providers;
 
+use App\Core\Enums\JobProtocolState;
+use App\Core\Module\Module;
 use App\Listeners\AuthUserLoginListener;
+use App\Models\JobProtocol;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Queue;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -29,9 +35,38 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Queue::before(function (JobProcessing $event) {
+            $uuid = $event->job->uuid();
+            $name = $event->job->resolveName();
+            $payload = $event->job->payload();
+            $description = null;
+            $moduleKey = null;
+
+            $jobClass = Arr::get($payload, 'data.commandName');
+            if ($jobClass) {
+                $job = new $jobClass();
+
+                if (method_exists($job, 'getModule')) {
+                    /** @var Module $module */
+                    $module = $job->getModule();
+                    $moduleKey = $module->getKey();
+                }
+
+                if (method_exists($job, 'getDescription')) {
+                    $description = @$job->getDescription();
+                }
+            }
+
+            JobProtocol::query()->create([
+                'uuid' => $uuid,
+                'name' => $name,
+                'description' => $description,
+                'module' => $moduleKey,
+                'state' => JobProtocolState::Create
+            ]);
+        });
     }
 
     /**
@@ -39,7 +74,7 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return bool
      */
-    public function shouldDiscoverEvents()
+    public function shouldDiscoverEvents(): bool
     {
         return false;
     }
