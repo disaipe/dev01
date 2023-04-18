28e7319bc1da853fc7d1c15e0cd37e41d7ce4db5
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-18 21:08:25 +0300

    feat: base reference view improvements

--- source/resources/js/views/dashboard/reference/BaseReference.vue
+++ source/resources/js/views/dashboard/reference/BaseReference.vue
@@ -6,17 +6,28 @@
         :can-update='canUpdate'
         :can-delete='canDelete'
     )
+        template(#columns-before)
+            vxe-column(v-if='hasRecordView' width='40px')
+                template(#default='{ row }')
+                    el-link(:underline='false' @click='editRecord(row.$getKey())')
+                        icon(icon='tabler:list-details' height='20')
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
 const reference = route.meta?.model;
 const canCreate = route.meta?.permissions?.create;
 const canUpdate = route.meta?.permissions?.update;
 const canDelete = route.meta?.permissions?.delete;
+const hasRecordView = !!route.meta?.recordView;
+
+const editRecord = (id) => {
+    router.push({ name: route.meta?.recordView, params: { id } });
+};
 </script>
 
 <script>
