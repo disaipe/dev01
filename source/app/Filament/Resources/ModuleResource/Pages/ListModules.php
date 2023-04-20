55c17525e067dcf63ba99bf9386fdef691244ab8
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-20 19:29:40 +0300

    fix: removed modules listing

--- source/app/Filament/Resources/ModuleResource/Pages/ListModules.php
+++ source/app/Filament/Resources/ModuleResource/Pages/ListModules.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\ModuleResource\Pages;
 
+use App\Core\Module\ModuleManager;
 use App\Filament\Resources\ModuleResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListModules extends ListRecords
 {
@@ -15,5 +17,14 @@ class ListModules extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        /** @var ModuleManager $modules */
+        $modules = app('modules');
+        $modulesKeys = array_keys($modules->getModules());
+
+        return parent::getTableQuery()->whereIn('key', $modulesKeys);
     }
 }
