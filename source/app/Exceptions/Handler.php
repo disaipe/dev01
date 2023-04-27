b6c3e939f5a94e248286460f2a43b3ac670cb9ac
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-04-27 19:43:30 +0300

    feat: add filament exceptions plugin

--- source/app/Exceptions/Handler.php
+++ source/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use BezhanSalleh\FilamentExceptions\FilamentExceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -44,7 +45,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if ($this->shouldReport($e)) {
+                FilamentExceptions::report($e);
+            }
         });
     }
 }
---
+++ source/lang/vendor/filament-exceptions/en/filament-exceptions.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'labels' => [
+        'model' => 'Исключение',
+        'model_plural' => 'Исключения',
+        'navigation' => 'Исключения',
+        'navigation_group' => 'Отладка',
+
+        'pills' => [
+            'exception' => 'Exception',
+            'headers' => 'Headers',
+            'cookies' => 'Cookies',
+            'body' => 'Body',
+            'queries' => 'Queries',
+        ],
+    ],
+
+    'empty_list' => 'Ура! Ошибок нет или их кто-то удалил 😎',
+
+    'columns' => [
+        'method' => 'Метод',
+        'path' => 'URL',
+        'type' => 'Типк',
+        'code' => 'Код',
+        'ip' => 'IP',
+        'occurred_at' => 'Дата',
+    ],
+
+];
