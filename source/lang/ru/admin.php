13606ad8133a0d066f0d7582511c3e9e8221156d
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-05-21 13:18:20 +0300

    chore: adapt AD module to cron expression input

--- source/lang/ru/admin.php
+++ source/lang/ru/admin.php
@@ -37,15 +37,9 @@ return [
     'started at' => 'Запущено',
     'ended at' => 'Закончено',
 
-    'cron helper' => '<pre class="text-xs ml-1">
- - - - - -
- | | | | |
- | | | | |
- | | | | +----- day of week (0 - 7) (Sunday=0 or 7)
- | | | +---------- month (1 - 12)
- | | +--------------- day of month (1 - 31)
- | +-------------------- hour (0 - 23)
- +------------------------- min (0 - 59)</pre>',
+    'last sync' => 'Последняя синхронизация',
+    'last sync date' => 'Последняя синхронизация: :date',
+    'next sync date' => 'Ближайшая синхронизация: :date',
 
     'menu' => [
         'access' => 'Доступ',
--- source/modules/ActiveDirectory/ADBaseServiceProvider.php
+++ source/modules/ActiveDirectory/ADBaseServiceProvider.php
@@ -2,18 +2,21 @@
 
 namespace App\Modules\ActiveDirectory;
 
+use App\Core\Enums\JobProtocolState;
 use App\Core\Indicator\Indicator;
 use App\Core\Indicator\IndicatorManager;
 use App\Core\Module\ModuleBaseServiceProvider;
 use App\Core\Reference\ReferenceManager;
-use App\Core\RegularExpressions;
 use App\Core\Report\Expression\CountExpression;
+use App\Filament\Components\CronExpressionInput;
 use App\Filament\Components\FormButton;
 use App\Forms\Components\RawHtmlContent;
 use App\Models\Domain;
+use App\Models\JobProtocol;
 use App\Modules\ActiveDirectory\Commands\LdapSync;
 use App\Modules\ActiveDirectory\Job\ADSyncJob;
 use App\Modules\ActiveDirectory\Models\ADEntry;
+use Cron\CronExpression;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Section;
@@ -67,6 +70,38 @@ class ADBaseServiceProvider extends ModuleBaseServiceProvider
             'description' => __('ad::messages.description'),
             'view' => [
                 'config' => [
+                    RawHtmlContent::make(function ($get) {
+                        $out = '';
+
+                        $lastSync = JobProtocol::query()
+                            ->where('name', '=', ADSyncJob::class)
+                            ->where('state', '=', JobProtocolState::Ready->value)
+                            ->orderByDesc('ended_at')
+                            ->first();
+
+                        if ($lastSync) {
+                            $out = '<div class="text-right text-sm">'.
+                                __('admin.last sync date', ['date' => $lastSync->ended_at]).
+                                '</div>';
+                        }
+
+                        $syncEnabled = $get('LdapSync.enabled');
+                        $schedule = $get('LdapSync.schedule');
+
+                        if ($syncEnabled && $schedule) {
+                            $expr = new CronExpression($schedule);
+                            $nextDate = $expr->getNextRunDate();
+                            $nextDateStr = $nextDate->format('Y-m-d H:i:s');
+
+                            $out .= '<div class="text-right text-sm">'.
+                                __('admin.next sync date', ['date' => $nextDateStr]).
+                                '</div>';
+                        }
+
+                        return $out;
+                    })
+                        ->columnSpanFull(),
+
                     Section::make(__('ad::messages.section_sync'))
                         ->schema([
                             Select::make('domain_id')
@@ -81,18 +116,16 @@ class ADBaseServiceProvider extends ModuleBaseServiceProvider
                             Textarea::make('filters')
                                 ->label(__('ad::messages.filter')),
                         ]),
+
                     Section::make(__('ad::messages.job.ldap sync.title'))
                         ->schema([
                             RawHtmlContent::make(__('ad::messages.job.ldap sync.description')),
 
+                            CronExpressionInput::make('LdapSync.schedule')
+                                ->disableLabel(),
+
                             Checkbox::make('LdapSync.enabled')
                                 ->label(__('admin.enabled')),
-
-                            TextInput::make('LdapSync.schedule')
-                                ->label(__('admin.schedule'))
-                                ->placeholder('* * * * * *')
-                                ->helperText(__('admin.cron helper'))
-                                ->regex(RegularExpressions::CRON),
 
                             FormButton::make('runJob')
                                 ->label(__('admin.run'))
