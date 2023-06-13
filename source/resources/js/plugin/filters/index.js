64681d4b5b60e9e3ed1c7270cce24afb3396ac57
Eugene Galitskiy <galitskiyem@digitaladvance.ru>
2023-06-13 19:37:46 +0300

    feat: additional bytes filters

--- source/resources/js/plugin/filters/index.js
+++ source/resources/js/plugin/filters/index.js
@@ -15,19 +15,47 @@ function formatDate(value, datetime = false) {
 }
 
 /**
- * Converts a number of bytes to a human-readable string.
+ * Converts a number of bytes to a human-readable string (metric)
  * @param {number} bytes The number of bytes to convert.
  * @param {number} [decimals=2] The number of decimal places to include in the result.
  * @returns {string} The number of bytes, formatted as a human-readable string (e.g. "1.23 MB").
  */
 function formatBytes(bytes, decimals= 2) {
-    if(bytes === 0) {
+    if (!bytes) {
+        return '0 Bytes';
+    }
+
+    const k = 1000;
+    const dm = decimals || 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+/**
+ * Converts a number of kbytes to a human-readable string (metric)
+ * @param {number} kbytes The number of kbytes to convert.
+ * @param {number} [decimals=2] The number of decimal places to include in the result.
+ * @returns {string} The number of kbytes, formatted as a human-readable string (e.g. "1.23 MB").
+ */
+function formatKBytes(kbytes, decimals = 2) {
+    return formatBytes(kbytes * 1000, decimals);
+}
+
+/**
+ * Converts a number of bytes to a human-readable string.
+ * @param {number} bytes The number of bytes to convert.
+ * @param {number} [decimals=2] The number of decimal places to include in the result.
+ * @returns {string} The number of bytes, formatted as a human-readable string (e.g. "1.23 MB").
+ */
+function formatBytes2(bytes, decimals= 2) {
+    if (!bytes) {
         return '0 Bytes';
     }
 
     const k = 1024;
     const dm = decimals || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
@@ -38,14 +66,16 @@ function formatBytes(bytes, decimals= 2) {
  * @param {number} [decimals=2] The number of decimal places to include in the result.
  * @returns {string} The number of kbytes, formatted as a human-readable string (e.g. "1.23 MB").
  */
-function formatKBytes(kbytes, decimals = 2) {
+function formatKBytes2(kbytes, decimals = 2) {
     return formatBytes(kbytes * 1024, decimals);
 }
 
 const filters = {
     formatDate,
     formatBytes,
-    formatKBytes
+    formatKBytes,
+    formatBytes2,
+    formatKBytes2
 };
 
 export default {
