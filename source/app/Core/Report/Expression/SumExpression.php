5608dffd9d689901beca7a970990479546f0aecd
disaipe <disaipe@gmail.com>
2023-12-14 19:23:32 +0300

    feat: sum expression field changed to Select

--- source/app/Core/Report/Expression/SumExpression.php
+++ source/app/Core/Report/Expression/SumExpression.php
@@ -2,10 +2,13 @@
 
 namespace App\Core\Report\Expression;
 
+use App\Core\Reference\ReferenceFieldSchema;
+use App\Core\Reference\ReferenceManager;
 use App\Core\Report\Expression;
 use App\Core\Report\IQueryExpression;
 use App\Forms\Components\RawHtmlContent;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Select;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Arr;
 
@@ -34,14 +37,47 @@ class SumExpression extends Expression implements IQueryExpression
     {
         return [
             RawHtmlContent::make(__('admin.$indicator.sum helper')),
-            TextInput::make('column')
+
+            Select::make('column')
                 ->label(__('admin.$indicator.column'))
-                ->required(),
+                ->options(fn (Component $component) => static::getReferenceColumns($component))
+                ->native(false)
+                ->required()
         ];
     }
 
     public static function disabled(array $state): bool
     {
         return false;
+    }
+
+    protected static function getReferenceColumns(Component $component): array
+    {
+        $parentState = $component
+            ->getContainer()
+            ->getParentComponent() // Block
+            ->getContainer()
+            ->getParentComponent() // Builder
+            ->getContainer()
+            ->getState();
+
+        $referenceName = Arr::get($parentState, 'schema.reference');
+
+        if ($referenceName) {
+            /** @var ReferenceManager $references */
+            $references = app('references');
+            $reference = $references->getByName($referenceName);
+
+            return collect($reference->getSchema())
+                ->mapWithKeys(function (ReferenceFieldSchema $field, string $key) {
+                    $label = $field->getAttribute('label');
+
+                    return [$key => $label ? "$label ($key)" : $key];
+                })
+                ->sort()
+                ->toArray();
+        }
+
+        return [];
     }
 }
