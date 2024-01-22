389aed7587d8c20b8d1d54031e169d1d20d126ca
disaipe <disaipe@gmail.com>
2024-01-22 20:03:10 +0300

    chore: change bytes converting filters

--- source/resources/js/plugin/filters/index.js
+++ source/resources/js/plugin/filters/index.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import numbro from 'numbro';
 
 /**
  * Format value to date or datetime string
@@ -22,14 +23,15 @@ function formatDate(value, datetime = false) {
  */
 function formatBytes(bytes, decimals= 2) {
     if (!bytes) {
-        return '0 Bytes';
+        return '0';
     }
 
-    const k = 1000;
-    const dm = decimals || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    return numbro(bytes).format({
+        output: 'byte',
+        base: 'binary',
+        spaceSeparated: true,
+        mantissa: decimals || 2
+    });
 }
 
 /**
@@ -42,40 +44,10 @@ function formatKBytes(kbytes, decimals = 2) {
     return formatBytes(kbytes * 1000, decimals);
 }
 
-/**
- * Converts a number of bytes to a human-readable string.
- * @param {number} bytes The number of bytes to convert.
- * @param {number} [decimals=2] The number of decimal places to include in the result.
- * @returns {string} The number of bytes, formatted as a human-readable string (e.g. "1.23 MB").
- */
-function formatBytes2(bytes, decimals= 2) {
-    if (!bytes) {
-        return '0 Bytes';
-    }
-
-    const k = 1024;
-    const dm = decimals || 2;
-    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
-
-/**
- * Converts a number of kbytes to a human-readable string.
- * @param {number} kbytes The number of kbytes to convert.
- * @param {number} [decimals=2] The number of decimal places to include in the result.
- * @returns {string} The number of kbytes, formatted as a human-readable string (e.g. "1.23 MB").
- */
-function formatKBytes2(kbytes, decimals = 2) {
-    return formatBytes(kbytes * 1024, decimals);
-}
-
 const filters = {
     formatDate,
     formatBytes,
-    formatKBytes,
-    formatBytes2,
-    formatKBytes2
+    formatKBytes
 };
 
 export default {
